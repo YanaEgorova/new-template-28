ad7e3f68a15eab5922aba515fac96041c58b9ab4
Yana <yanaegorova69.3@gmail.com>
2021-09-22 02:29:30 +0300

    +

--- js/data/products.js
+++ js/data/products.js
@@ -1,152 +1,146 @@
 export const products = [
     {
         id: 'product-1',
-        name: 'Lazy Glasses Prism',
+        name: 'Soap Dish with Drain',
         price: 6.96,
         image: './img/img1.png',
         description: [
             {
-                text: 'Used for both youngsters and old people,great for the bedridden and hospital patients.',
+                title: 'Maximizes The Life Of Your Soap',
+                text: 'Slotted design can exactly let soap drain and dry naturally, which will preserve the life of your luxury soap. And the underneath container will keep back the water, not leaks onto the surface beneath it, keep your sink surface dry.',
             },
             {
-                title: 'Benefit for Health',
-                text: 'Lazy Glasses help you read books and watch TV shows without direct contact and moving your head. Prevents from neck pain, radiation and eyestrain.',
+                title: 'Made With Durable Plastic',
+                text: 'Even when dropped, they don\'t break or crack like the cheap plastic ones you can get from dollar stores.',
             },
             {
-                title: 'Super Convenient',
-                text: 'This High-qualified Optical Glasses is in a light weight which can be carried all around easily. And It has the similar shape as the ordinary one. You can wear the prism glasses only or wear it over your ordinary glasses (include prescription glasses).',
+                title: 'Perfect Size',
+                text: 'Soap case size is 5.1" x 3.7" x 1.1", and the slotted part is high enough that soap does\'t sit in water and get mushy, extends the life of your soap. Perfect for bathroom and sink.',
             },
             {
-                title: 'High Quality Glasses',
-                text: 'Lazy Glasses are made by High Definition glasses and high quality reflecting prism which bring Clear and bright image, enable people in a supine position to see object in the front.',
+                title: 'Safe And Non-Toxic',
+                text: 'Our soap holder is zero volatile organic compounds (VOCs), which is important these days with an increasing number of people having allergies and sensitivities - our soap dish is not just a soap container but a health saver too.',
+            },
+            {
+                title: 'Note',
+                text: 'No lid/cover for this soap container. This soap holder has two levels, the top container being removable, easy to take it apart to clean.',
             },
         ],
         images: ['./img/img1-1.png', './img/img1-2.png'],
     },
     {
         id: 'product-2',
-        name: 'Yoga Mat',
+        name: 'Adult Swim Goggles',
         price: 29.99,
         image: './img/img2.png',
         description: [
             {
-                title: 'All-Around Exercise Mat',
-                text: 'Yoga Mat is longer and thicker than traditional yoga mats, providing extra room and space for added security as you move through your flow. Pismo measures 72” x 24” x .5mm and rolls into compact cylinder for easy travel.',
+                text: 'Comfortable flat strap with single point adjustment.',
             },
             {
-                title: 'Find Balance',
-                text: 'No sliding, slipping, or losing your balance. Moisture-resistant, ribbed texture stays in one place and prevents slipping, keeping you firmly grounded and balanced during your practice.',
+                text: 'Unibody frame.',
             },
             {
-                title: 'Comfort & Control',
-                text: 'Yoga Mat perfectly balances cushioning and firmness to encourage comfortable movement without sacrificing control. Mat quickly regains its shape even after prolonged applied pressure.',
-            },
-            {
-                title: 'Free Of Harsh Chemicals',
-                text: 'Vegan and PVC free. Mat is expertly designed for optimal performance during all yoga practices.',
-            },
-            {
-                title: 'Easy To Clean',
-                text: 'Mat is easy to keep clean and fresh for your next workout. Use a mix of gentle soap and water on any of our bright color options, wipe clean, and hang it to dry. Do not submerge.',
+                text: 'Anti fog, polycarbonate lenses.',
             },
         ],
-        images: ['./img/img2-1.png', './img/img2-2.jpg', './img/img2-3.png', './img/img2-4.png', './img/img2-5.png'],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png'],
     },
     {
         id: 'product-3',
-        name: 'Mop Bucket on Wheels',
+        name: 'Baby Dome',
         price: 64.90,
         image: './img/img3.png',
         description: [
             {
-                text: '35-quart mop bucket and side-press wringer combo for cleaning floors or mopping up messes; designed for commercial use.',
+                text: 'A play space and a comfy napping spot, perfect for when you and baby are on the go.',
             },
             {
-                text: 'High-efficiency side-press wringer works with fan mop holders; down-press wringer works with fan and jaw mop holders; wringers quickly send water back down into bucket.',
+                text: 'Can be used indoors or out. For babies 0-5 months.',
             },
             {
-                text: 'Made of durable polypropylene; measurement marks (in 5-quart increments) run up the inside wall of the bucket.',
+                text: 'Canopy protects your little one from the sun and bugs.',
             },
             {
-                text: 'Loop carry handle; integrated pour handle; 4 smooth-rolling casters for quiet, convenient mobility; bright yellow color for easy visibility and added safety.',
+                text: '2 removable toys.',
             },
             {
-                text: 'Mop-handle clip-on holder keeps mop upright in bucket; mop not included.',
+                text: 'Folds flat, with handles for easy take-along.',
             },
         ],
-        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png', './img/img3-4.png', './img/img3-5.png'],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png', './img/img3-4.png', './img/img3-5.png', './img/img3-6.png', './img/img3-7.png', './img/img3-8.png'],
     },
     {
         id: 'product-4',
-        name: 'Discreet Protective Hemp Backpack',
+        name: 'Multi-Shelf',
         price: 98.90,
         image: './img/img4.png',
         description: [
             {
-                title: 'Padded Glass Protection',
-                text: 'This duffle tube has a soft padded interior designed to keep glassware and accessories safe. It\'s shed-proof velour padded lining keeps your accessories undamaged and protected while you\'re on-the-go. While this bag is great for pipe storage, it has many other uses as well! You can use this as a makeup bag, pencil pouch, or zipper pouch for almost any accessories you can think of.',
+                title: 'Modern And Eye-Catching Multi-Shelf',
+                text: 'This unique floating shelf adds a modern touch to complete your space while providing versatile shelving and storage.',
             },
             {
-                title: 'Plenty Of Storage',
-                text: 'This bag has plenty of space to store all of your belongings. Along with an extremely spacious internal compartment, the bag also an external mesh water bottle pocket for easy to access storage. The tube\'s soft shoulder straps can be adjusted for the perfect fit.',
+                title: 'Wall Decor & Storage',
+                text: 'Shelf includes two versatile and removable beech wood planters that can be used to show off your favorite greenery or store a variety of office or household items.',
             },
             {
-                title: 'Eco Friendly Hemp Fabric',
-                text: 'Our blend of hemp and recycled polyester maintains all of the eco friendly and sustainable features of pure hemp with an improved smooth and soft feel. The hemp fabric is designed to resist fading, so colors last. The tough hemp blend also resists tearing, ripping and other physical damage.',
+                title: 'Customizable For Your Space',
+                text: 'Shelves can be moved around to fit your preference and measures 25 x 6. 44 x 19 inches (63. 5 x 16 x 48 cm).',
             },
             {
-                title: 'Removable Smell-Proof Stash Baggie',
-                text: 'Comes with a clear plastic smell proof bag that is both removable and smell-proof for neat and discreet transportation. The heavy duty zipper keeps your things safe and secure. The durable zipper is designed to avoid binding, biting and separation. The zipper makes it easy to open the tube when you want to and keeps it firmly closed when you don\'t.',
+                title: 'Easy To Set Up & Use',
+                text: 'Your floating shelf comes complete with simple step-by-step installation instructions and all the necessary hardware and brackets to easily mount it in just minutes.',
             },
         ],
-        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png'],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png', './img/img4-5.png'],
     },
     {
         id: 'product-5',
-        name: 'Diagonal Cable Tire Chains',
+        name: 'Storage Small Cabinet',
         price: 119.98,
         image: './img/img5.png',
         description: [
             {
-                text: 'Diagonal cross cable tire chains design provides a smooth ride and consistent traction.',
+                text: 'Plastic.',
             },
             {
-                text: 'Easy installation without having to move the vehicle. Compatible with ABS and Traction Control.',
+                text: 'Resin construction is durable and tough and virtually maintenance-free. Unlike wooden or metal, it will not rot, rust, dent, or peel.',
             },
             {
-                text: 'Rubber Tensioners and Carrying bag included. Sold per pair.',
+                text: 'Lockable doors keep valuables out of sight and hazardous chemicals safely away from children. (Lock not included).',
             },
             {
-                text: 'Fits: 8-19.5 9-17.5 11.00-15 31x11.50-15 31x11.50-16.5 31x13.50-15 31x13.5-16.5 32x10.50-17 33x10.50-16.5 215/75-19.5 225/75-19.5 225/75-20 235/80-17 245/70-18 245/85-16 255/50-21 255/55-20 255/60-19 255/65-18 255/65-19 255/70-17 255/70-18 255/75-15 265/50-20 265/65-18 265/70-17 265/75-16 265/80-16 275/45-21',
+                text: 'It has one ventilated shelf and one solid shelf. Shelving cannot be adjusted or removed.',
             },
             {
-                text: 'Fits: 255/75-16 275/70-22 275/45-22 275/50-20 275/55-19 275/60-18 275/65-17 27570-16 275/70-17 275/75-15 275/75-16 285/55-18 285/55-19 295/60-17 285/65-16 285/70-15 285/75-15 295/30-23 295/35-22 295/40-21 295/45-20 305/35-19 305/35-20 305/35-22 305/55-18 315/35-20 325/60-15',
+                text: '13.5 cubic feet of storage space. Holds up to 336 pounds. Made in the USA.',
             },
         ],
-        images: ['./img/img5-1.png', './img/img5-2.jpg'],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png'],
     },
     {
         id: 'product-6',
-        name: 'Piano For Kids',
+        name: 'Speed Bag',
         price: 148.00,
         image: './img/img6.png',
         description: [
             {
-                text: 'Piano produces grand sound to accompany its grand appearance.',
+                text: 'Height adjustable design for individual needs.',
             },
             {
-                text: 'This piano has a realistic look, excellent resonant sound, and a lid that can be opened.',
+                text: '24-inch diameter with 1.5 inch thick board for stabilization.',
             },
             {
-                text: 'Promotes dexterity, hand-eye coordination, musical skills, and creativity.',
+                text: 'Professional grade design.',
             },
             {
-                text: 'Recommened for ages 36 months and up.',
+                text: 'Kit includes: speed bag (10" x 7"), speed bag swivel and hand pump with needle.',
             },
             {
-                text: 'Non-toxic finishes and child safe materials.',
+                text: 'Wall mount hardware not included.',
             },
         ],
-        images: ['./img/img6-1.png','./img/img6-2.png', './img/img6-3.png', './img/img6-4.png', './img/img6-5.png', './img/img6-6.png', './img/img6-7.png', './img/img6-8.png'],
+        images: ['./img/img6-1.png','./img/img6-2.png', './img/img6-3.png', './img/img6-4.png'],
     },
 ]
