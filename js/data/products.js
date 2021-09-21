2a94eece1082c0a08c0a6d61fb255f9d6f7e8f4d
Yana <yanaegorova69.3@gmail.com>
2021-09-21 23:49:57 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,152 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Lazy Glasses Prism',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Used for both youngsters and old people,great for the bedridden and hospital patients.',
+            },
+            {
+                title: 'Benefit for Health',
+                text: 'Lazy Glasses help you read books and watch TV shows without direct contact and moving your head. Prevents from neck pain, radiation and eyestrain.',
+            },
+            {
+                title: 'Super Convenient',
+                text: 'This High-qualified Optical Glasses is in a light weight which can be carried all around easily. And It has the similar shape as the ordinary one. You can wear the prism glasses only or wear it over your ordinary glasses (include prescription glasses).',
+            },
+            {
+                title: 'High Quality Glasses',
+                text: 'Lazy Glasses are made by High Definition glasses and high quality reflecting prism which bring Clear and bright image, enable people in a supine position to see object in the front.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Yoga Mat',
+        price: 29.99,
+        image: './img/img2.png',
+        description: [
+            {
+                title: 'All-Around Exercise Mat',
+                text: 'Yoga Mat is longer and thicker than traditional yoga mats, providing extra room and space for added security as you move through your flow. Pismo measures 72” x 24” x .5mm and rolls into compact cylinder for easy travel.',
+            },
+            {
+                title: 'Find Balance',
+                text: 'No sliding, slipping, or losing your balance. Moisture-resistant, ribbed texture stays in one place and prevents slipping, keeping you firmly grounded and balanced during your practice.',
+            },
+            {
+                title: 'Comfort & Control',
+                text: 'Yoga Mat perfectly balances cushioning and firmness to encourage comfortable movement without sacrificing control. Mat quickly regains its shape even after prolonged applied pressure.',
+            },
+            {
+                title: 'Free Of Harsh Chemicals',
+                text: 'Vegan and PVC free. Mat is expertly designed for optimal performance during all yoga practices.',
+            },
+            {
+                title: 'Easy To Clean',
+                text: 'Mat is easy to keep clean and fresh for your next workout. Use a mix of gentle soap and water on any of our bright color options, wipe clean, and hang it to dry. Do not submerge.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.jpg', './img/img2-3.png', './img/img2-4.png', './img/img2-5.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Mop Bucket on Wheels',
+        price: 64.90,
+        image: './img/img3.png',
+        description: [
+            {
+                text: '35-quart mop bucket and side-press wringer combo for cleaning floors or mopping up messes; designed for commercial use.',
+            },
+            {
+                text: 'High-efficiency side-press wringer works with fan mop holders; down-press wringer works with fan and jaw mop holders; wringers quickly send water back down into bucket.',
+            },
+            {
+                text: 'Made of durable polypropylene; measurement marks (in 5-quart increments) run up the inside wall of the bucket.',
+            },
+            {
+                text: 'Loop carry handle; integrated pour handle; 4 smooth-rolling casters for quiet, convenient mobility; bright yellow color for easy visibility and added safety.',
+            },
+            {
+                text: 'Mop-handle clip-on holder keeps mop upright in bucket; mop not included.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png', './img/img3-4.png', './img/img3-5.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Discreet Protective Hemp Backpack',
+        price: 98.90,
+        image: './img/img4.png',
+        description: [
+            {
+                title: 'Padded Glass Protection',
+                text: 'This duffle tube has a soft padded interior designed to keep glassware and accessories safe. It\'s shed-proof velour padded lining keeps your accessories undamaged and protected while you\'re on-the-go. While this bag is great for pipe storage, it has many other uses as well! You can use this as a makeup bag, pencil pouch, or zipper pouch for almost any accessories you can think of.',
+            },
+            {
+                title: 'Plenty Of Storage',
+                text: 'This bag has plenty of space to store all of your belongings. Along with an extremely spacious internal compartment, the bag also an external mesh water bottle pocket for easy to access storage. The tube\'s soft shoulder straps can be adjusted for the perfect fit.',
+            },
+            {
+                title: 'Eco Friendly Hemp Fabric',
+                text: 'Our blend of hemp and recycled polyester maintains all of the eco friendly and sustainable features of pure hemp with an improved smooth and soft feel. The hemp fabric is designed to resist fading, so colors last. The tough hemp blend also resists tearing, ripping and other physical damage.',
+            },
+            {
+                title: 'Removable Smell-Proof Stash Baggie',
+                text: 'Comes with a clear plastic smell proof bag that is both removable and smell-proof for neat and discreet transportation. The heavy duty zipper keeps your things safe and secure. The durable zipper is designed to avoid binding, biting and separation. The zipper makes it easy to open the tube when you want to and keeps it firmly closed when you don\'t.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Diagonal Cable Tire Chains',
+        price: 119.98,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Diagonal cross cable tire chains design provides a smooth ride and consistent traction.',
+            },
+            {
+                text: 'Easy installation without having to move the vehicle. Compatible with ABS and Traction Control.',
+            },
+            {
+                text: 'Rubber Tensioners and Carrying bag included. Sold per pair.',
+            },
+            {
+                text: 'Fits: 8-19.5 9-17.5 11.00-15 31x11.50-15 31x11.50-16.5 31x13.50-15 31x13.5-16.5 32x10.50-17 33x10.50-16.5 215/75-19.5 225/75-19.5 225/75-20 235/80-17 245/70-18 245/85-16 255/50-21 255/55-20 255/60-19 255/65-18 255/65-19 255/70-17 255/70-18 255/75-15 265/50-20 265/65-18 265/70-17 265/75-16 265/80-16 275/45-21',
+            },
+            {
+                text: 'Fits: 255/75-16 275/70-22 275/45-22 275/50-20 275/55-19 275/60-18 275/65-17 27570-16 275/70-17 275/75-15 275/75-16 285/55-18 285/55-19 295/60-17 285/65-16 285/70-15 285/75-15 295/30-23 295/35-22 295/40-21 295/45-20 305/35-19 305/35-20 305/35-22 305/55-18 315/35-20 325/60-15',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.jpg'],
+    },
+    {
+        id: 'product-6',
+        name: 'Piano For Kids',
+        price: 148.00,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Piano produces grand sound to accompany its grand appearance.',
+            },
+            {
+                text: 'This piano has a realistic look, excellent resonant sound, and a lid that can be opened.',
+            },
+            {
+                text: 'Promotes dexterity, hand-eye coordination, musical skills, and creativity.',
+            },
+            {
+                text: 'Recommened for ages 36 months and up.',
+            },
+            {
+                text: 'Non-toxic finishes and child safe materials.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png', './img/img6-3.png', './img/img6-4.png', './img/img6-5.png', './img/img6-6.png', './img/img6-7.png', './img/img6-8.png'],
+    },
+]
